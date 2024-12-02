17ce4d5fef680c0fcadbadd5a8aa53844b89dbcd
Ali <ali.subhani@tintash.com>
2024-12-02 16:07:11 +0200

    update  cors configurations

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
